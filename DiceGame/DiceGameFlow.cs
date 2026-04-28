6e1e9a4943e6773cf9c565fb508f3f915928770a
allenxue <fdafdaq@gmail.com>
2026-04-28 11:41:50 +0100

    feat: Implement beginning of game logic, the roll dice function and check the value equals to 1.

--- DiceGame/DiceGameFlow.cs
+++ DiceGame/DiceGameFlow.cs
@@ -1,4 +1,5 @@
 using DiceGame.Player; 
+using DiceGame;
 
 namespace DiceGame;
 
@@ -14,6 +15,27 @@ public class DiceGameFlow()
 
        Player player1 = new Player(player1Name);
        Player player2 = new Player(player2Name);
+       Player currentPlayer = player1;
+
+       int rollScore = 0;
+       int currentTurnScore = 0;
+       bool gameOver = false;
+
+       while (!gameOver)
+        {
+            Console.WriteLine($"It is player {currentPlayer.Name}'s turn. Please roll dice by press enter.");
+            Console.ReadLine();
+
+            rollScore = Dice.RollDice();
+
+            if (rollScore == 1)
+            {
+                Console.WriteLine("you rolled a 1, tyou lose all unheld scores and your turn ends.");
+                currentTurnScore = 0;
+                currentPlayer = currentPlayer == player1? player2: player1;
+                continue;
+            }
+        }
        
     }
     
