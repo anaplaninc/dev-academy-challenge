ab2bfda5bfb82c33bfea5df8edd64b5ddeb3c4bb
TasianJoseph <tasianpjoseph@gmail.com>
2026-04-28 11:35:42 +0100

    fleshing out player and rolling dice

--- dice_game.py
+++ dice_game.py
@@ -9,26 +9,30 @@ print("Welcome to my 2 player roll dice game. Let's get started!\n")
 player1 = input("Please enter your name player 1: \n")
 print(f"Welcome {player1}")
 
-if not player1:
-    print(f"Ok, we'll call you player 1")
+# if not player1:
+#    print(f"Ok, we'll call you player 1")
 
 
 player2 = input("Please enter your name player 2: \n")
 print(f"Welcome {player2}")
 
-if not player2:
-    print(f"Ok, we'll call you player 2 \n")
+# if not player2:
+#    print(f"Ok, we'll call you player 2 \n")
 
-# 3.
+# 3.STARTING THE GAME, BOTH PLAYERS START ON 0 POINTS :
+print(input(f"{player1} will go first, press enter to continue ..."))
+
+player1_game_score = 0
+player2_game_score = 0
 
 # 4. DICE ROLLING MECHANIC, FED TO TURN SCORE VARIABLE AND RULE FOR ROLLING A 1:
 dice_roll = random.randint(1,6)
-print(dice_roll)
-
 turn_score = dice_roll
+
+print(f"You have rolled a: {dice_roll}. And your turn score is {turn_score}.")
 
 
 if dice_roll == 1:
-    print("End of turn")
+    print("You rolled a 1. End of turn.")
 else:
-    print("Would you like to h")
+    print(input(f"Would you like to bank points?, if "))
