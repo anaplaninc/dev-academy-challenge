4f5de838a961a7cdeef158da7868cbeffa2f8442
Ryan Tidswell <tidsyy@gmail.com>
2026-04-28 10:53:08 +0100

    Added starting game variables

--- javascript-starter-code/script.js
+++ javascript-starter-code/script.js
@@ -12,10 +12,22 @@ const player0NameElement = document.getElementById('name--0');
 const player1NameElement = document.getElementById('name--1');
 
 //Buttons
-const rollBtn = document.getElementById('btn--roll')
-const holdBtn = document.getElementById('btn--hold')
-const resetBtn = document.getElementById('btn--new')
+const rollBtn = document.getElementById('btn--roll');
+const holdBtn = document.getElementById('btn--hold');
+const resetBtn = document.getElementById('btn--new');
 
 // Starting conditions
 score0Element.textContent = 0;
 score1Element.textContent = 0;
+
+//Game Config/Variables
+//Stored inside an object so we can access all required game variables using game.(variable name)
+const game = {
+  players: [ //Tracks our players, using an array would potentially allow for additional players to be added
+    { name: 'Player 2', total: 0 },
+    { name: 'Player 2', total: 0 },
+  ],
+  currentScore: 0, //Tracks the current score
+  currentPlayer: 0, //Tracks the index of the current player, which we can use in the 'players' array
+  isGameOver: false, //Tracks whether the game is over e.g. has someone won
+};
