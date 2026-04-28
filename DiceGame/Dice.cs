578e01be3a1232508ac1cc5c7740a1048044ba6d
allenxue <fdafdaq@gmail.com>
2026-04-28 11:58:33 +0100

    chore: add some comments

--- DiceGame/Dice.cs
+++ DiceGame/Dice.cs
@@ -4,6 +4,7 @@ public class Dice
 {
     private static Random rand = new Random();
 
+    //A static method that give a random number out of given range
     public static int RollDice()
     {
         return rand.Next(1, 7);
--- DiceGame/Player.cs
+++ DiceGame/Player.cs
@@ -2,9 +2,11 @@ namespace DiceGame;
 
 public class Player()
 {
+    //Properties that the player class hold
     public string Name { get; set; }
-    public int TotalScore { get; set; }
+    public int TotalScore { get; set; } = 0;
 
+    //constructor that requires a string to define name;
     public Player(string name)
     {
         Name = name;
