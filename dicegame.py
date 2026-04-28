5d2eebc6e6c634ee88cad91b584b0f3f5517fd3f
Jaden <jadenfearon4@outlook.com>
2026-04-28 10:59:43 +0100

    added random library

---
+++ dicegame.py
@@ -0,0 +1,6 @@
+import random
+
+
+print("Welcome to the Dice Game! First to reach 100 or more points wins. There are 2 players. if a player rolls a 1 they lose any unheld score from that turn and the next player goes." \
+      " If a player rolls a 2-6 they can choose to hold their score or roll again. If they hold, their unheld score is added to their total score and the next player goes.")
+
