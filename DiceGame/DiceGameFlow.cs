7e2ef8cbdc7cbfb44b6ce9e1561ef9dc9fce68bb
allenxue <fdafdaq@gmail.com>
2026-04-28 11:34:27 +0100

    feat: Implement add 2 player logic

--- DiceGame/DiceGameFlow.cs
+++ DiceGame/DiceGameFlow.cs
@@ -1,6 +1,20 @@
+using DiceGame.Player; 
+
 namespace DiceGame;
 
 public class DiceGameFlow()
 {
+
+    public void Play()
+    {
+       Console.WriteLine("Please enter first player name:"); 
+       string player1Name = Console.ReadLine();
+       Console.WriteLine("please enter second player name:");
+       string player2Name = Console.ReadLine();
+
+       Player player1 = new Player(player1Name);
+       Player player2 = new Player(player2Name);
+       
+    }
     
 }
--- DiceGame/Player.cs
+++ DiceGame/Player.cs
@@ -4,4 +4,10 @@ public class Player()
 {
     public string Name { get; set; }
     public int TotalScore { get; set; }
+
+    public Player(string name)
+    {
+        Name = name;
+        TotalScore = 0;
+    }
 }
