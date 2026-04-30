86aa138eec613a244b462320da86e2f7bd194441
OwenLister-GitHub <owenl9.ol@gmail.com>
2026-04-30 11:05:59 +0100

    Add Player contructors

--- Player.java
+++ Player.java
@@ -1,4 +1,15 @@
+import java.util.*;
+
 public class Player{
 
-    
+    String name;
+    List<String> presetNames = new ArrayList<String>(Arrays.asList("Player 1", "Player 2"));
+
+    void Player(Integer playerNumber){
+        name = presetNames.get(playerNumber);
+    }
+
+    void Player(String name){
+        this.name = name;
+    }
 }
