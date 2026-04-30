a1140b1ceb316e50b2526fc61b33a635c04991ce
OwenLister-GitHub <owenl9.ol@gmail.com>
2026-04-30 11:32:29 +0100

    Add functionality for Game to start

--- Game.java
+++ Game.java
@@ -14,16 +14,28 @@ public class Game {
         dice.Roll();
 
         while(game.score1 < 100 || game.score2 < 100){
-
+            Integer rollScore = game.takeTurn(dice, p1);
+            game.bank(p1, rollScore);
+            rollScore = game.takeTurn(dice, p2);
+            game.bank(p2, rollScore);
         }
     }
 
-    public void bank(Player player){
-        
+    public void bank(Player player, Integer turnScore){
+        player.totalScore += turnScore;
     }
 
-    public void takeTurn(Dice dice, Player player){
+    public Integer takeTurn(Dice dice, Player player){
         Integer roll = dice.Roll();
         player.turnScore = roll;
+        return roll;
+    }
+
+    public void resetGame(){
+
+    }
+
+    public void newGame(){
+
     }
 }
