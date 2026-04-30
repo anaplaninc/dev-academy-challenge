8dd735d42bbe0953946bcdbb48e6d50507cca3fe
OwenLister-GitHub <owenl9.ol@gmail.com>
2026-04-30 11:26:15 +0100

    Add bank and turn methods

--- Game.java
+++ Game.java
@@ -2,14 +2,28 @@ public class Game {
     Integer score1 = 0;
     Integer score2 = 0;
 
+    public Game(){
+
+    }
+
     public static void main(String[] args){
+        Game game = new Game();
         Player p1 = new Player(1);
         Player p2 = new Player(2);
         Dice dice = new Dice();
         dice.Roll();
+
+        while(game.score1 < 100 || game.score2 < 100){
+
+        }
     }
 
-    public void takeTurn(){
+    public void bank(Player player){
         
+    }
+
+    public void takeTurn(Dice dice, Player player){
+        Integer roll = dice.Roll();
+        player.turnScore = roll;
     }
 }
--- Player.java
+++ Player.java
@@ -3,13 +3,31 @@ import java.util.*;
 public class Player{
 
     String name;
+    Integer turnScore = 0;
+    Integer totalScore = 0;
     List<String> presetNames = new ArrayList<String>(Arrays.asList("Player 1", "Player 2"));
 
-    void Player(Integer playerNumber){
-        name = presetNames.get(playerNumber);
+    public Player(){
+
     }
 
-    void Player(String name){
+    public Player(Integer playerNumber){
+        name = presetNames.get(playerNumber-1);
+    }
+
+    public Player(String name){
         this.name = name;
     }
+
+    public void bank(Integer heldScore){
+        totalScore = heldScore;
+    }
+
+    // public void SetTurnScore(Integer turnScore){
+    //     this.turnScore = turnScore;
+    // }
+
+    // public Integer GetTurnScore(){
+    //     return this.turnScore;
+    // }
 }
